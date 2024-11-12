2d8f59e37409d4ca9ea9606b398f9316e8bf62d0
nandayekhaungkyaw <nandayekhaungkyaw>
2024-11-12 21:09:51 +0630

    test-nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+// Import images directly
+import logo from '../photo/logo.gif';
+import cv from '../photo/cv.gif';
+
 const NavBar = () => {
     useEffect(() => {
         AOS.init({
@@ -20,7 +24,7 @@ const NavBar = () => {
                         data-aos="fade-right"
                         data-aos-delay="200"
                     >
-                        <img src="./src/photo/logo.gif" className="size-12" alt="Flowbite Logo" />
+                        <img src={logo} className="size-12" alt="Flowbite Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                             Nanda
                         </span>
@@ -33,7 +37,7 @@ const NavBar = () => {
                         data-aos-delay="300"
                     >
                         <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2">
-                            <img src="./src/photo/cv.gif" className="size-6 mr-4" alt="" />
+                            <img src={cv} className="size-6 mr-4" alt="Download CV" />
                             Download CV
                         </button>
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
---
+++ src/photo/index.js
@@ -0,0 +1,3 @@
+export { default as logo } from './logo.png';
+export { default as cv } from './cv.png';
+// Add more exports as needed
